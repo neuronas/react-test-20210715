cb6d671e19f1a657ec59e62cc22c878a22936469
orlan <orlandonuske@gmail.com>
2021-07-15 11:43:08 -0300

    make elements toggle when clicked and add some styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Board from './components/Board'
 function App() {
   return (
     <div className="container">
-      <div className="app">
+      <div className="main">
           <Board />
       </div>
     </div>
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 
+
 const Board = ({locations}) => {
 
 	let array = Array.from(Array(10).keys())
+
+	let onClick = (e) => {
+		let elem = e.target
+		// toggle
+		elem.innerHTML = elem.innerText === "-" ? 'o' : '-'
+	}
 
 	return (
 		<div className="board">
@@ -12,7 +19,7 @@ const Board = ({locations}) => {
 					<span key={k}> 	
 						{
 							array.map((k,v) => (
-									'-'
+									<div className="node" key={k} onClick={onClick}>-</div>
 								)
 							)
 						}
